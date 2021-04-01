4821af9345003b859bcf0040351136ea60b71799
Emiel de Jong <emieldejong@xs4all.nl>
2021-04-01 08:45:34 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { REASONS } from './mock-reasons';
+import { Reason } from './reason';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'dropdown-test';
+export class AppComponent implements OnInit {
+  reasons = REASONS;
+
+  ngOnInit(): void {
+    // this.sortByTitle();
+    // this.topReasons = this.getMostUsed(4);
+  }
+
+  sortByTitle(reasons: Reason[]) {
+    const sortedReasons = [...reasons];
+
+    sortedReasons.sort((a, b) => {
+      if (a.title < b.title) {
+        return -1;
+      }
+      if (a.title > b.title) {
+        return 1;
+      }
+      return 0;
+    });
+
+    return sortedReasons;
+  }
+
+  getTopReasons(reasons: Reason[], numberOfResults: number) {
+    const sortedReasons = [...reasons];
+    return this.sortByUsageCount(sortedReasons).slice(0, numberOfResults);
+  }
+
+  private sortByUsageCount(reasons: Reason[]) {
+    const sortedReasons = [...reasons];
+    return sortedReasons.sort((a, b) => b.usageCount - a.usageCount);
+  }
 }

135d70a330c439e0c5a49405a5c869f9e1ee6937
Emiel de Jong <emieldejong@xs4all.nl>
2021-04-07 09:38:58 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,24 +21,14 @@ export class AppComponent implements OnInit {
     const localReasons = JSON.parse(localStorage.getItem('reasons'));
     if (!this.containsObject(reason, localReasons)) {
       localReasons.unshift(reason);
-      // localStorage.setItem(
-      //   'reasons',
-      //   JSON.stringify(localReasons.slice(0, this.numberOfTopReasons))
-      // );
     } else {
-      console.log('localReasons', localReasons);
-
-      // console.log(localReasons[0].id);
-
       const index = localReasons.findIndex((r) => r.id === reason.id);
-      console.log('index', index);
 
+      // switch order, put last used on top of the list
       [localReasons[0], localReasons[index]] = [
         localReasons[index],
         localReasons[0],
       ];
-
-      console.log('localReasons', localReasons);
     }
     localStorage.setItem(
       'reasons',
@@ -62,7 +52,7 @@ export class AppComponent implements OnInit {
     return sortedReasons;
   }
 
-  getTopReasons(reasons: Reason[], numberOfResults: number) {
+  getTopReasons() {
     return JSON.parse(localStorage.getItem('reasons'));
   }
 
